6ab4c8279bbd9290299034037da131754896c769
Muchiri Mwangi <muchirimwangi09@gmail.com>
2025-03-31 01:02:58 +0300

    Replace Footer component with a new implementation in Resources directory; maintain existing layout and functionality.

--- src/Resources/Footer.jsx
+++ src/Resources/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       </a>
 
       <span class="block text-sm text-center text-gray-500">© 2025 Content Creator Insurance™. All Rights Reserved. Built By 
-          <a href="https://muchiri-mwangi-com.vercel.app/" class="text-yellowGreen hover:underline"> Muchiri Mwangi</a>.
+          <a href="https://muchiri-the-dev.vercel.app/" class="text-yellowGreen hover:underline"> Muchiri Mwangi</a>.
       </span>
 
       <ul class="flex justify-center mt-5 space-x-5">
