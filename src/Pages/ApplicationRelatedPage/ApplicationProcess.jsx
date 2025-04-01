9f933d59faa025388acf6ef361e91734a1ba6d71
Muchiri Mwangi <muchirimwangi09@gmail.com>
2025-04-02 01:48:06 +0300

    Add InsuranceDetails component with form and coverage information; update ApplicationProcess to include it

--- src/Pages/ApplicationRelatedPage/ApplicationProcess.jsx
+++ src/Pages/ApplicationRelatedPage/ApplicationProcess.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import PersonalDetails from '../../Component/ApplicationComponents/PersonalDetails'
 import ContentAccountInformation from '../../Component/ApplicationComponents/ContentAccountInformation'
+import InsuranceDetails from '../../Component/ApplicationComponents/InsuranceDetails'
 
 const ApplicationProcess = () => {
   return (
     <div>
       {/* <PersonalDetails /> */}
-      <ContentAccountInformation />
+      {/* <ContentAccountInformation /> */}
+      <InsuranceDetails />
     </div>
   )
 }
