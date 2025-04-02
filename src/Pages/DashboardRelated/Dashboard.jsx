3a20ba180a06452052d94c3196795d2957c09961
Muchiri Mwangi <muchirimwangi09@gmail.com>
2025-04-02 12:17:41 +0300

    Add welcome message to Dashboard component

--- src/Pages/DashboardRelated/Dashboard.jsx
+++ src/Pages/DashboardRelated/Dashboard.jsx
@@ -340,6 +340,7 @@ const Dashboard = () => {
         transition={{ duration: 1, delay: 0.4 }}
         className="flex-1 ml-0 md:ml-64 mt-16 p-4 md:p-8 overflow-y-auto h-[calc(100vh-4rem)]"
       >
+        <h1 className='text-2xl font-bold py-6 w-full bg-white mb-4 px-5 mt-1 rounded-lg shadow-xl'> Welcome, Muchiri Mwangi</h1>
         <div className="max-w-full md:max-w-7xl mx-auto">
           {sections[activeSection]}
         </div>
