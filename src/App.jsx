e27e38c110093b1bff62904c4ff076ae81cc40ab
Muchiri Mwangi <muchirimwangi09@gmail.com>
2025-05-08 11:47:11 +0300

    feat: Implement ErrorBoundary component for improved error handling in the application

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Inpage404 from './Resources/Inpage404.jsx'
 import ResetEmail from './Pages/AuthRelatedPage/ResetEmail.jsx'
 import PasswordReset from './Pages/AuthRelatedPage/PasswordReset.jsx'
 import AddPlatform from './Pages/DashboardRelated/AddPlatform.jsx'
+import ErrorBoundary from './Resources/ErrorBoundary.jsx'
 
 export const backendUrl = `${import.meta.env.VITE_BACKEND_URL}/api`
 console.log(backendUrl)
@@ -26,48 +27,50 @@ function App() {
 
   return (
     <div className="">
-      <Routes>
-        <Route path='/' element={<LandingPage/>}/>
-        <Route path='/application' element={<ApplicationProcess/>}/>
-        <Route path='/signup' element={<SignUp/>}/>
-        <Route path='/login' element={<Login />}/>
-        <Route path='/claim' element={<ClaimProcess/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/verify-email' element={<VerificationNotification/>}/>
-        <Route path='/verify/:token' element={<EmailVerify/>}/>
-        <Route path='/reset-email' element={<ResetEmail/>}/>
-        <Route path='/reset-password/:token' element={<PasswordReset/>}/>
-        <Route path='/dashboard/add-platform' element={<AddPlatform/>}/>
-        <Route path='*' element={<Inpage404/>}/>
-      </Routes>
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-        toastOptions={{
-          className: '',
-          duration: 3000,
-          style: {
-            background: '#333',
-            color: '#4F391A',
-            fontSize: '16px',
-            padding: '10px',
-            borderRadius: '7px',
-            border: '1px solid #AAC624',
-          },
-          success: {
+      <ErrorBoundary>
+        <Routes>
+          <Route path='/' element={<LandingPage/>}/>
+          <Route path='/application' element={<ApplicationProcess/>}/>
+          <Route path='/signup' element={<SignUp/>}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/claim' element={<ClaimProcess/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/verify-email' element={<VerificationNotification/>}/>
+          <Route path='/verify/:token' element={<EmailVerify/>}/>
+          <Route path='/reset-email' element={<ResetEmail/>}/>
+          <Route path='/reset-password/:token' element={<PasswordReset/>}/>
+          <Route path='/dashboard/add-platform' element={<AddPlatform/>}/>
+          <Route path='*' element={<Inpage404/>}/>
+        </Routes>
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          toastOptions={{
+            className: '',
+            duration: 3000,
             style: {
-              background: '#fff',
+              background: '#333',
               color: '#4F391A',
+              fontSize: '16px',
+              padding: '10px',
+              borderRadius: '7px',
+              border: '1px solid #AAC624',
             },
-          },
-          error: {
-            style: {
-              background: '#fff',
-              color: '#4F391A',
+            success: {
+              style: {
+                background: '#fff',
+                color: '#4F391A',
+              },
             },
-          },
-        }}
-      />
+            error: {
+              style: {
+                background: '#fff',
+                color: '#4F391A',
+              },
+            },
+          }}
+        />
+      </ErrorBoundary>
     </div>
   )
 }
